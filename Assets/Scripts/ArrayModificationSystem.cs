18839a2c779de689e0839a6bf3b5936728c05676
DevFenrils <nesfenrils@gmail.com>
2023-08-21 19:42:37 +0200

    add ArrayModificationSystem
    revisa los cambios @RaulGal

---
+++ Assets/Scripts/ArrayModificationSystem.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class ArrayModificationSystem : MonoBehaviour
+{
+    public Book destinationObjects;
+    public Sprite[] ruina1Objects;
+    public Sprite[] ruina2Objects;
+
+    public int[] counters = new int[8];
+    public Sprite[] countersSprites;
+    public void Start()
+    {
+        countersSprites = destinationObjects.bookPages;
+        counters = new int[countersSprites.Length];
+        for (int i = 0; i < countersSprites.Length; i++)
+        {
+            counters[i] = 1;
+        }
+    }
+
+    public void IncrementCounter(int index)
+    {
+        counters[index]++;
+
+        UpdateObjectAtIndex(index);
+
+    }
+
+    private void UpdateObjectAtIndex(int index)
+    {
+
+        if (index < 0 || index >= counters.Length)
+        {
+            Debug.LogError("Invalid index.");
+            return;
+        }
+
+        Sprite objectToInstantiate = GetObjectForCounter(counters[index], index);
+
+        if (objectToInstantiate != null)
+        {
+            Destroy(countersSprites[index]);
+            countersSprites[index] = objectToInstantiate;
+        }
+    }
+
+    private Sprite GetObjectForCounter(int counter, int index)
+    {
+        if (index < 0 || index >= countersSprites.Length)
+        {
+            Debug.LogWarning("Invalid index.");
+            return null;
+        }
+
+        Sprite objectToInstantiate = null;
+
+        if (counters[index] > 0)
+        {
+            switch (index)
+            {
+                case 1:
+                    objectToInstantiate = ruina1Objects[counter];
+                    break;
+                case 2:
+                    objectToInstantiate = ruina2Objects[counter];
+                    break;
+
+                default:
+                    Debug.LogWarning("No object assigned for counter value: " + counter);
+                    break;
+            }
+        }
+
+        return objectToInstantiate;
+    }
+}
--- Assets/Scripts/Selected.cs
+++ Assets/Scripts/Selected.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class Selected : MonoBehaviour
 {
+    public ArrayModificationSystem modificationSystem;
     LayerMask mask;
     public float distancia = 2f;
     public Texture2D puntero;
@@ -14,8 +15,6 @@ public class Selected : MonoBehaviour
     public GameObject TextTalk;
     public GameObject TextE;
     public float factor = 0.5f;
-
-    // Start is called before the first frame update
 
     void Start()
     {
@@ -28,40 +27,34 @@ public class Selected : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.green, distancia);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distancia)) {
-                Debug.Log(hit.collider.tag);
-            if (hit.collider.tag == "InteractNote") {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distancia))
+        {
+            if (hit.collider.tag == "InteractPista1")
+            {
 
-                    TextE.SetActive(true);
-                if (Input.GetKeyDown(KeyCode.E) ||  Input.touchCount == 1  && Input.GetTouch(0).phase == TouchPhase.Began) {
+                TextE.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E) || Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+
+                    int indexToIncrement = 1;
+                    modificationSystem.IncrementCounter(indexToIncrement);
+                    hit.collider.tag = "Untagged";
                 }
 
-           /* } else if (hit.collider.tag == "talk") {
-
-                if (hit.collider.transform.GetComponent<MovementPeasants>().speed > 0) {
-                    TextTalk.SetActive(true);
-
-                } else {
-                    TextTalk.SetActive(false);
-
-                }
-
-                if (Input.GetKeyDown(KeyCode.E) ||  Input.touchCount == 1  && Input.GetTouch(0).phase == TouchPhase.Began) {
-                    hit.collider.transform.GetComponent<MovementPeasants>().Talk();
-                }   */
-                        
-            } else {
+            }
+            else
+            {
                 TextE.SetActive(false);
             }
-        } else {
-                TextE.SetActive(false);
+        }
+        else
+        {
+            TextE.SetActive(false);
         }
     }
 }
 
-    
